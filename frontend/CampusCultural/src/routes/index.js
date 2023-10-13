e401e42d7edcf609236947defa5248f291108fcd
felipe <felipe.shimada@kepha.com.br>
2023-10-13 12:38:21 -0300

    Home web

--- frontend/CampusCultural/src/routes/index.js
+++ frontend/CampusCultural/src/routes/index.js
@@ -5,6 +5,8 @@ import Home from '../pages/Home';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Text, View, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons'
+import Calendario from '../pages/Calendario';
+import Perfil from '../pages/Perfil';
 
 const Stack = createNativeStackNavigator()
 const Tab = createBottomTabNavigator()
@@ -52,6 +54,46 @@ function AppNav() {
                         >
                             <Ionicons name="home" size={size} color='white' />
                             <Text style={styles.iconText}>INÍCIO</Text>
+                        </View>
+                    }
+
+                }}
+            />
+
+            <Tab.Screen
+                name='Calendario'
+                component={Calendario}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size, focused }) => {
+                        return <View
+                            style={[
+                                styles.iconContainer,
+                                focused && styles.iconContainerFocused
+                            ]}
+                        >
+                            <Ionicons name="calendar" size={size} color='white' />
+                            <Text style={styles.iconText}>CALENDÁRIO</Text>
+                        </View>
+                    }
+
+                }}
+            />
+
+            <Tab.Screen
+                name='Perfil'
+                component={Perfil}
+                options={{
+                    headerShown: false,
+                    tabBarIcon: ({ color, size, focused }) => {
+                        return <View
+                            style={[
+                                styles.iconContainer,
+                                focused && styles.iconContainerFocused
+                            ]}
+                        >
+                            <Ionicons name="person" size={size} color='white' />
+                            <Text style={styles.iconText}>PERFIL</Text>
                         </View>
                     }
 
